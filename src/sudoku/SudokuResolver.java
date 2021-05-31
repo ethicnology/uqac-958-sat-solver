48755335a887bcd0daa06f6ff322f714d289201f
Dicosaedrique <abouaban2@gmail.com>
2021-05-31 17:21:38 +0200

    add condition 1

--- src/sudoku/SudokuResolver.java
+++ src/sudoku/SudokuResolver.java
@@ -2,6 +2,11 @@ package sudoku;
 
 import java.util.regex.Pattern;
 
+import stev.booleans.And;
+import stev.booleans.BooleanFormula;
+import stev.booleans.Implies;
+import stev.booleans.Not;
+import stev.booleans.Or;
 import stev.booleans.PropositionalVariable;
 
 /**
@@ -35,6 +40,12 @@ class SudokuResolver {
 		// create variables
 		initVariables();
 		
+		BooleanFormula[] conditions1 = this.generateCondition1();
+		
+		for(int i = 0; i < conditions1.length; i++) {
+			System.out.println(conditions1[i]);
+		}
+		
 		return this.input;
 	}
 	
@@ -49,6 +60,41 @@ class SudokuResolver {
 				}
 			}
 		}
+	}
+	
+	/**
+	 * Generate condition 1 (1 cell can only contain 1 value)
+	 * @return
+	 */
+	private BooleanFormula[] generateCondition1() {
+		BooleanFormula[] formulas = new BooleanFormula[81];
+		
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
+				
+				int index = i * 9 + j;
+				
+				Implies impliesForCell[] = new Implies[10];
+				
+				for(int value = 0; value < 10; value++) {
+					Not notForValue[] = new Not[9];
+					
+					int actualK = 0;
+					for(int k = 0; k < 10; k++) {
+						if(k == value) continue;
+						
+						notForValue[actualK] = new Not(this.variables[i][j][k]);
+						actualK++;
+					}
+							
+					impliesForCell[value] = new Implies(this.variables[i][j][value], new And(notForValue));
+				}
+				
+				formulas[index] = new Or(impliesForCell);
+			}
+		}
+		
+		return formulas;
 	}
 	
 	
